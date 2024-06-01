415425ff5fbcc7d3141d27abf82a96b8af6aa00d
Schalk Pienaar <pienaarsw@gmail.com>
2024-06-01 22:45:27 +1200

    added solution and api project

---
+++ Evently/src/API/Evently.Api/Program.cs
@@ -0,0 +1,15 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwagger();
+}
+
+app.Run();
+
