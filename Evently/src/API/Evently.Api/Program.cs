301eb5cefcf89af5a540526a6a99152e1f70432b
Schalk Pienaar <pienaarsw@gmail.com>
2024-06-04 13:58:18 +1200

    Add caching service and redis container

--- Evently/src/API/Evently.Api/Program.cs
+++ Evently/src/API/Evently.Api/Program.cs
@@ -20,7 +20,9 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
 
-builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!); 
+builder.Services.AddInfrastructure(
+    builder.Configuration.GetConnectionString("Database")!,
+    builder.Configuration.GetConnectionString("Cache")!); 
 
 builder.Configuration.AddModuleConfiguration(["events"]);
 
